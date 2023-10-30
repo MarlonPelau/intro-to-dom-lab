e5bb8f98d5d0acdf8dcec55e0b924b062eb8b5fc
MarlonPelau <140419493+MarlonPelau@users.noreply.github.com>
2023-10-30 12:37:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// console.log("Yo from the main.js file!");
+// document.querySelector("h2").innerText = "Yo Bro, Se fe!";
+const section = document.createElement("section");
+section.classList.add("featured");
+
+const article = document.createElement("article");
+
+const header = document.createElement("a");
+header.remove(`href="#">All Posts`);
+
+const span = document.createElement("span");
+span.querySelector.remove();
+
+const lastPost = document.createElement("p");
+lastPost.remove(`You -- yes you! You're an over-planner, I can tell. It's time to stop
+planning so much and instead focusing on relaxing. Taking a break at all is
+so stressful these days; why add to the stress by overworking yourself?`);
+
+span.querySelectorAll.remove();
+
+
+
+
+
+const sectionPosts = document.createElement("section.posts");
+
+
